61c33606741dc32f2a2eb9a9bcbd65c0f584ab19
kasarama <cph-mw216@cphbusiness.dk>
2022-05-15 12:11:37 +0200

    first entities and utils

---
+++ data_service/src/main/java/db/utils/AddressFetcher.java
@@ -0,0 +1,7 @@
+package db.utils;
+
+public class AddressFetcher {
+    /*todo
+    populate Addresses with data from https://api.dataforsyningen.dk/adresser?struktur=mini&kommunekode=0159
+     */
+}
---
+++ data_service/src/main/java/db/utils/MenuFetcher.java
@@ -0,0 +1,8 @@
+package db.utils;
+
+public class MenuFetcher {
+    /*todo
+    find some sample menus and populate DB
+     */
+
+}
