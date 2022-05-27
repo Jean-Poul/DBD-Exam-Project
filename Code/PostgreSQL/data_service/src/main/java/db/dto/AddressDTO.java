f9e382f3ef7e0da641a504ddcc451876c41613c1
Jean-Poul <56347744+Jean-Poul@users.noreply.github.com>
2022-05-27 20:29:56 +0200

    Get restaurant coordinates (#15)
    * longitude and latitude on
    
    * Service urls added
    
    * Service urls added v2
    
    Co-authored-by: Magdalena Wawrzak <54578672+kasarama@users.noreply.github.com>

--- Code/PostgreSQL/data_service/src/main/java/db/dto/AddressDTO.java
+++ Code/PostgreSQL/data_service/src/main/java/db/dto/AddressDTO.java
@@ -8,6 +8,18 @@ public class AddressDTO {
     private String street;
     private int zipcode;
 
+    private double x;
+    private double y;
+
+    public AddressDTO(String building, String local, String street, int zipcode, double x, double y) {
+        this.building = building;
+        this.local = local;
+        this.street = street;
+        this.zipcode = zipcode;
+        this.x = x;
+        this.y = y;
+    }
+
     public AddressDTO(Address address) {
         this.building = address.getBuildingIdentifier();
         this.local = address.getLocalIdentifier();
@@ -29,5 +41,13 @@ public class AddressDTO {
 
     public int getZipcode() {
         return zipcode;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
     }
 }
