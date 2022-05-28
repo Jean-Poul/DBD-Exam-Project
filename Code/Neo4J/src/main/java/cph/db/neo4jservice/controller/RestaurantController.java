3d1231fb53065b88155539e5d56ad5bdcccb0635
AllanSimonsen789 <allanalli@hotmail.com>
2022-05-28 02:24:34 +0200

    Neo4j Service Done

--- Code/Neo4J/src/main/java/cph/db/neo4jservice/controller/RestaurantController.java
+++ Code/Neo4J/src/main/java/cph/db/neo4jservice/controller/RestaurantController.java
@@ -1,13 +1,21 @@
 package cph.db.neo4jservice.controller;
 
-import cph.db.neo4jservice.entities.MovieEntity;
 import cph.db.neo4jservice.entities.RestaurantEntity;
-import cph.db.neo4jservice.repository.MovieRepository;
 import cph.db.neo4jservice.repository.RestaurantRepository;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.summary.ResultSummary;
+import org.springframework.data.neo4j.core.Neo4jClient;
+import org.springframework.data.neo4j.core.ReactiveNeo4jClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/restaurant")
@@ -33,13 +41,65 @@ public class RestaurantController {
     Flux<RestaurantEntity> byCityName(@RequestParam String cityName) {
         return restaurantRepository.findRestaurantsByCityName(cityName);
     }
-
-    @GetMapping("/populate-database")
-    void populateDB() {
-       restaurantRepository.populateDatabase();
+    @GetMapping("/by-delivery")
+    Flux<RestaurantEntity> byDelivery(@RequestParam String delivery) {
+        return restaurantRepository.findRestaurantsByDelivery(delivery);
     }
-    /*@DeleteMapping("/\{id\}")
-    Mono<Void> delete(@PathVariable String id) {
-        return movieRepository.deleteById(id);
-    }*/
+    @GetMapping("/by-food")
+    Flux<RestaurantEntity> byFoodType(@RequestParam String foodCategory) {
+        return restaurantRepository.findRestaurantsByFoodCategory(foodCategory);
+    }
+
+    @GetMapping("/get-similar-restaurants")
+    Flux<RestaurantEntity> similarRestaurants(@RequestParam String id) {
+            Driver driver = GraphDatabase
+                    .driver("bolt://localhost:7687", AuthTokens.basic("neo4j", ""));
+
+            Neo4jClient client = Neo4jClient.create(driver);
+            try {
+                Collection<Map<String, Object>> allActors = client
+                        .query("CALL gds.graph.project(\n" +
+                                "    'restaurantSimilarity',\n" +
+                                "    ['Restaurant', 'FoodCategory', 'Delivery', 'Address' ],\n" +
+                                "    {\n" +
+                                "        HAS: {\n" +
+                                "            type: 'HAS',\n" +
+                                "            properties: {\n" +
+                                "                strength: {\n" +
+                                "                    property: 'category',\n" +
+                                "                    defaultValue: 0.2\n" +
+                                "                }\n" +
+                                "            }\n" +
+                                "        },\n" +
+                                "        LOCATION: {\n" +
+                                "            type: 'LOCATION',\n" +
+                                "            properties: {\n" +
+                                "                strength: {\n" +
+                                "                    property: 'zipcode',\n" +
+                                "                    defaultValue: 1.0\n" +
+                                "                }\n" +
+                                "            }\n" +
+                                "        },\n" +
+                                "        DELIVERS: {\n" +
+                                "            type: 'DELIVERS',\n" +
+                                "            properties: {\n" +
+                                "                strength: {\n" +
+                                "                    property: 'delivery',\n" +
+                                "                    defaultValue: 1.0\n" +
+                                "                }\n" +
+                                "            }\n" +
+                                "        }\n" +
+                                "    }\n" +
+                                ")\n" +
+                                "YIELD nodeCount\n" +
+                                "RETURN nodeCount")
+                        .in("neo4j")
+                        .fetch()
+                        .all();
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        return restaurantRepository.findSimilarRestaurants(id);
+    }
+
 }
