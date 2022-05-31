6cbb388f1a90b93728e6efbaaa2504226eee1d7f
AllanSimonsen789 <allanalli@hotmail.com>
2022-05-31 13:20:07 +0200

    Fixed import of data

--- Code/Neo4J/src/main/java/cph/db/neo4jservice/controller/RestaurantController.java
+++ Code/Neo4J/src/main/java/cph/db/neo4jservice/controller/RestaurantController.java
@@ -50,6 +50,63 @@ public class RestaurantController {
         return restaurantRepository.findRestaurantsByFoodCategory(foodCategory);
     }
 
+    @GetMapping("/populate_db")
+    String similarPopulate() {
+        Driver driver = GraphDatabase
+                .driver("bolt://localhost:7687", AuthTokens.basic("neo4j", ""));
+
+        Neo4jClient client = Neo4jClient.create(driver);
+        try {
+            ResultSummary restaurants = client
+                    .query("LOAD CSV WITH HEADERS FROM 'file:///restaurants_202205222339.csv'\n" +
+                            "AS restaurants\n" +
+                            "MERGE (r:Restaurant{restaurant_name:(restaurants.restaurant_name), pg_id:(restaurants.id), user_id:(restaurants.user_id), address_id:(restaurants.address_id)})\n" +
+                            "MERGE (d:Delivery{delivery:(restaurants.delivery)})\n" +
+                            "CREATE (r)-[rel:DELIVERS]->(d);")
+                    .in("neo4j").run();
+            System.out.println(restaurants);
+            ResultSummary addresses = client
+                    .query("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/Jean-Poul/DBD-Exam-Project/developer/data/addresses_202205222339.csv'\n" +
+                            "AS addresses\n" +
+                            "MATCH (r:Restaurant{address_id:(addresses.id)})\n" +
+                            "CALL {\n" +
+                            "    WITH addresses, r\n" +
+                            "    MERGE (a:Address{id:(addresses.id), building_id:(addresses.building_id), local_id:(addresses.local_id), street:(addresses.street), x:(addresses.x), y:(addresses.y), zipcode:(addresses.zipcode)})\n" +
+                            "    CREATE (r)-[rel:LOCATION]->(a)\n" +
+                            "}\n")
+                    .in("neo4j").run();
+            System.out.println(addresses);
+            ResultSummary zipcodes = client
+                    .query("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/Jean-Poul/DBD-Exam-Project/developer/data/zipcode_clean.csv'\n" +
+                            "AS zipcodes\n" +
+                            "MERGE (zip:Zipcode{zipcode:(zipcodes.postnummer), city_name:(zipcodes.by)});")
+                    .in("neo4j").run();
+            System.out.println(zipcodes);
+            ResultSummary zipcodessec = client
+                    .query("MATCH\n" +
+                            "  (zip:Zipcode),\n" +
+                            "  (add:Address)\n" +
+                            "WHERE zip.zipcode = add.zipcode\n" +
+                            "CREATE (add)-[r:LOCATED_IN]->(zip)")
+                    .in("neo4j").run();
+            System.out.println(zipcodessec);
+            ResultSummary items = client
+                    .query("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/Jean-Poul/DBD-Exam-Project/developer/data/items.csv'\n" +
+                            "AS items\n" +
+                            "MATCH (r:Restaurant{pg_id:(items.restaurant_id)})\n" +
+                            "CALL {\n" +
+                            "    WITH items, r\n" +
+                            "    MERGE (it:FoodCategory{category:(items.category)})\n" +
+                            "    MERGE (r)-[rel:HAS]->(it)\n" +
+                            "}")
+                    .in("neo4j").run();
+            System.out.println(items);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return "Database populated succesfully";
+    }
+
     @GetMapping("/get-similar-restaurants")
     Flux<RestaurantEntity> similarRestaurants(@RequestParam String id) {
             Driver driver = GraphDatabase
@@ -57,7 +114,7 @@ public class RestaurantController {
 
             Neo4jClient client = Neo4jClient.create(driver);
             try {
-                Collection<Map<String, Object>> allActors = client
+                Collection<Map<String, Object>> runGDS = client
                         .query("CALL gds.graph.project(\n" +
                                 "    'restaurantSimilarity',\n" +
                                 "    ['Restaurant', 'FoodCategory', 'Delivery', 'Address' ],\n" +
